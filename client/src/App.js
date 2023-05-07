f472923c79964cd250ac32b9a43f70b18bf58b4c
unknown <jord_ogrady@hotmail.com>
2023-05-08 03:27:59 +1000

    Added mutations and queries to the frontend and fixed App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,39 @@
-import Register from "./Pages/Register/index";
-import Home from "./Pages/Home/home";
-import Login from "./componets/Login/Login";
-import Header from "./componets/Header/index";
-import Chats from "./Pages/Chats/Chats";
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { setContext } from "@apollo/client/link/context";
-import "./index.css";
+import Register from './pages/Register/Register';
+import Home from './pages/Home/Home';
+import Signin from './pages/Signin/Signin';
+import Landing from './pages/Landing/Landing';
+import Header from './componets/Header/Header'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
-const httpLink = createHttpLink({ uri: "http://localhost:3001/graphql" });
+
+// const httpLink = createHttpLink({ uri: '/graphql'})
+const httpLink = createHttpLink({ uri: 'http://localhost:3001/graphql'})
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
+  const token = localStorage.getItem('id_token');
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
+      authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
 
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+const client = new ApolloClient({ link: authLink.concat(httpLink), cache: new InMemoryCache() })
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={client} >
       <div className="App">
         <Router>
           <Header />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/Chats" element={<Chats />} />
+            <Route path= "/" element={<Landing/>}/>
+            <Route path= "/register" element={<Register/>}/>
+            <Route path= "/signin" element={<Signin/>}/>
+            <Route path= "/home" element={<Home/>}/>
           </Routes>
         </Router>
       </div>
