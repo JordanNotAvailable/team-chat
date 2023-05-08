7a32db6255f4cffd688f8b5940c7e40b51f36cdc
unknown <jord_ogrady@hotmail.com>
2023-05-09 01:58:04 +1000

    Refactored Socket code and broke it up and repathed to required areas

--- client/src/componets/ChatNav/ChatNav.js
+++ client/src/componets/ChatNav/ChatNav.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { useState } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import Modal from '../FriendModal/FriendModal'
+import ChatContent from '../ChatContent/ChatContent';
 import './ChatNav.css';
 
-export default function ChatNav() {
+export default function ChatNav({ setOpen, chatActive, socket, onOpen, groupchat }) {
+
+  const [messages, setMessages] = useState([]);
+  const [typingStatus, setTypingStatus] = useState('');
+  const lastMessageRef = useRef(null);
 
   const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    socket.on('messageResponse', (data) => setMessages([...messages, data]));
+  }, [socket, messages]);
+
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
 
   const toggle = () => {
     setModal(!modal);
@@ -20,11 +32,28 @@ export default function ChatNav() {
             <input type="text" placeholder="Search chat name" />
           </div>
           <div className="button-group">
+            <button onClick={() => onOpen()}>Create group chat</button>
             <button>Create chat</button>
             <button onClick={toggle}>Add friend</button>
             {modal ? <Modal toggle={toggle} /> : null}
           </div>
         </div>
+        {chatActive ?
+          <ChatContent socket={socket} groupchat={groupchat} />
+          :
+          <div className="flex-grow flex flex-col">
+            <div className="flex justify-center items-center h-10">
+              <input
+                type="text"
+                placeholder="Search group name"
+              />
+            </div>
+            <div className="flex-grow flex-col flex justify-center items-center">
+              <p>Create Chat by using Create Chat Button</p>
+              <p>Add Friend by using Add user as friend</p>
+            </div>
+          </div>
+        }       
       </div>
     </div>
     </>
