59b51fe7c8bf28cca3145103e31351ae324d9df6
unknown <jord_ogrady@hotmail.com>
2023-05-09 21:14:25 +1000

    changes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import Register from './pages/Register/Register';
 import Home from './pages/Home/Home';
 import Signin from './pages/Signin/Signin';
 import Landing from './pages/Landing/Landing';
 import SPR from './pages/SPR/SPR'
+import Register from './pages/Register/Register';
 import Header from './componets/Header/Header'
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
